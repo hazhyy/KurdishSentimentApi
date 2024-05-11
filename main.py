308e3295cb24dbaa8988ae296531481ca5be2fe5
hazhyy <hazhyy@gmail.com>
2024-05-12 01:48:08 +0300

    added score endpoint

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from joblib import load
 from gensim.models import Word2Vec
 from wordvector import averageVector
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -15,8 +17,17 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+
+class TextModel(BaseModel):
+    text: str
+    model: str
+
+
+stopwords_path = "./wordvector/stop_words.txt"
 word2vec_model = Word2Vec.load("./wordvector/kurdish-word2vec-75279.model")
-lr_loaded = load("./models/LogisticModel9000.joblib")
+rf_sentiment = load("./models/LogisticModel40000.joblib")
+lr_loaded = load("./models/LogisticModel40000.joblib")
+lr_score = load("./models/logisticModelSentiment.joblib")
 
 
 class Text(BaseModel):
@@ -40,16 +51,64 @@ sentiments = {
 }
 
 
+def get_word_predictions(text: str, model: str):
+    with open(stopwords_path, "r", encoding="utf-8") as f:
+        stopwords = f.read().splitlines()
+    words = text.split()  # Split the text into words
+    word_vectors = []
+    print(model)
+    for word in words:
+        if word not in stopwords:  # Check if the word is not in stopwords
+            vector = averageVector.get_average_word_vector(
+                word, word2vec_model, 100, stopwords_path=stopwords_path
+            )
+            if model == "lr":
+                prediction = lr_loaded.predict([vector])
+            elif model == "rf":
+                prediction = rf_sentiment.predict([vector])
+            word_vectors.append(
+                {
+                    "word": word,
+                    "prediction_class": int(prediction[0]),
+                    "sentiment": sentiments[int(prediction[0])]["text"],
+                }
+            )
+    return word_vectors
+
+
 @app.post("/predict/")
-async def predict_sentiment(text: Text):
+async def predict_sentiment(text_model: TextModel):
     try:
+        word_vectors = get_word_predictions(text_model.text, text_model.model)
+
         new_vector = averageVector.get_average_word_vector(
-            text.text, word2vec_model, 100
+            text_model.text, word2vec_model, 100, stopwords_path=stopwords_path
         )
-        prediction = lr_loaded.predict([new_vector])
+
+        if text_model.model == "lr":
+            prediction = lr_loaded.predict([new_vector])
+        elif text_model.model == "rf":
+            prediction = rf_sentiment.predict([new_vector])
         return {
-            "prediction": int(prediction[0]),
-            "text": sentiments[prediction[0]]["text"],
+            "labeled_prediction_class": int(prediction[0]),
+            "labeled_sentiment": sentiments[prediction[0]]["text"],
+            "labeled_prediction_by_word": word_vectors,
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/score/")
+async def predict_score(text: Text):
+    try:
+
+        new_vector = averageVector.get_average_word_vector(
+            text.text, word2vec_model, 100, stopwords_path=stopwords_path
+        )
+
+        prediction = lr_score.predict([new_vector])
+        return {
+            "score": int(prediction[0]),
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- wordvector/averageVector.py
+++ wordvector/averageVector.py
@@ -2,13 +2,27 @@ from gensim.models import Word2Vec
 import numpy as np
 
 
-def get_average_word_vector(sentence, model, num_features):
-    """Calculate the average word vector for a sentence."""
+def get_average_word_vector(
+    sentence,
+    model,
+    num_features,
+    stopwords_path="./stop_words.txt",
+):
+    """Calculate the average word vector for a sentence, excluding stop words."""
+    # Load stop words from file
+    try:
+        with open(stopwords_path, "r", encoding="utf-8") as file:
+            stopwords = set(file.read().split())
+    except Exception as e:
+        print(f"Failed to open file: {e}")
     words = sentence.split()
     feature_vector = np.zeros((num_features,), dtype="float32")
     num_words = 0
     for word in words:
-        if word in model.wv.key_to_index:
+        if (
+            word not in stopwords and word in model.wv.key_to_index
+        ):  # Check if word is not a stop word
+            print(word)
             num_words += 1
             feature_vector = np.add(feature_vector, model.wv[word])
     return feature_vector / num_words if num_words > 0 else feature_vector
